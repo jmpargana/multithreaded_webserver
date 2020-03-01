7706078338d4950195c43b3dca3a5716440af131
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-03-02 00:06:30 +0100

    added small documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,22 @@ use std::thread;
 use std::sync::{mpsc, Arc, Mutex};
 
 
+/// Implements the thread pool concept.
+///
+/// It contains a group of pre-instantiated, idle threads
+/// which stand ready to be given work
 pub struct ThreadPool {
+    /// It must contain a list of workers
     workers: Vec<Worker>,
+    /// And a Sender connecting to each of the threads
     sender: mpsc::Sender<Message>,
 }
 
+/// Type alias of a smart function pointer with 
+/// Send trait and static lifetime
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+/// Contains either a Job or a Terminate message
 enum Message {
     NewJob(Job),
     Terminate,
@@ -17,7 +26,17 @@ enum Message {
 impl ThreadPool {
     /// Create a new ThreadPool.
     ///
-    /// The size is the number of threads in the pool.
+    /// # Arguments
+    ///
+    /// * `size` - is the number of threads in the pool.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use multithreaded_webserver::ThreadPool;
+    ///
+    /// let pool = ThreadPool::new(5);
+    /// ```
     ///
     /// # Panics
     ///
@@ -42,6 +61,21 @@ impl ThreadPool {
         }
     }
 
+    /// Spawns a thread by sendin the worker a message with a job
+    ///
+    /// # Arguments
+    ///
+    /// * `f` -> Function of generic type
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use multithreaded_webserver::ThreadPool;
+    ///
+    /// let pool = ThreadPool::new(5);
+    ///
+    /// pool.execute(|| println!("Spawned thread executing this closure!"));
+    /// ```
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
@@ -53,6 +87,10 @@ impl ThreadPool {
 }
 
 impl Drop for ThreadPool {
+    /// Implement Drop Trait for ThreadPool
+    ///
+    /// It avoid all threads to instantly close as soon as drop is called
+    /// instead, while a process is being run, that thread waits
     fn drop(&mut self) {
         println!("Sending terminate message to all workers.");
 
@@ -72,12 +110,22 @@ impl Drop for ThreadPool {
     }
 }
 
+/// Connects the thread with others with the join handle
 struct Worker {
+    /// Also contains an id to be shown to user 
     id: usize,
     thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
+    /// Creates new instance of Worker
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - Number from 0 to n given to worker
+    /// * `receiver` - Its an atomic reference counter containing a mutex
+    /// which contains the receiving end of a mpsc in ThreadPool and receives
+    /// a Message enum contianing either a Terminate value or function call
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
